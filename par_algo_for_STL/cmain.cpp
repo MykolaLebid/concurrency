76fc86595534bc8facfa77a1ac4d4195e296411b
MykolaLebid <mykola.lebid@gmail.com>
2020-01-05 23:49:15 +0200

    parallel algo

---
+++ par_algo_for_STL/cmain.cpp
@@ -0,0 +1,13 @@
+#include <algorithm>
+#include <execution>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+int main(){
+	std::cout << std::endl;
+
+	std::vector<int> myVec{1,2,3,4,5};
+	return 0;
+}
